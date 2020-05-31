a30a371d34d64e38f3edce4b2d6ed42569939d6c
Abhishek <abhisheksinghal7599@gmail.com>
2020-05-31 17:30:29 +0530

    second commit

--- twigger/urls.py
+++ twigger/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='twigger-home'),
+    # path('', views.home, name='twigger-home'),
+    path('', PostListView.as_view(), name='twigger-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name = 'twigger-about')
     #path('about', views.about, name = 'twigger-about')
 ]
--- twigger/views.py
+++ twigger/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+
 
 def home(request):
     context = {
         'posts': Post.objects.all(),
     }
     return render(request, 'twigger/home.html', context)
+
+# class based view with same functionality as home view
+class PostListView(ListView):
+    model = Post 
+    template_name = 'twigger/home.html'  # <app>/<model>_<viewtype>.html  this is django convention
+    # we named our template differently so we need to set this variable
+    context_object_name = 'posts' # by default it is called objectList, we have diffrent name so we need to set it here
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'twigger/about.html', {'title': 'About Twigger'})
