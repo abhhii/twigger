e7be08031b13cc1253db61d523fabf093a9afbe6
Abhishek <abhisheksinghal7599@gmail.com>
2020-05-31 20:58:25 +0530

    Pagination for user pages

--- twigger/urls.py
+++ twigger/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     # path('', views.home, name='twigger-home'),
     path('', PostListView.as_view(), name='twigger-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
--- twigger/views.py
+++ twigger/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import (
     ListView,
     DetailView,
@@ -24,6 +25,19 @@ class PostListView(ListView):
     context_object_name = 'posts' # by default it is called objectList, we have diffrent name so we need to set it here
     ordering = ['-date_posted']
     paginate_by = 2
+
+class UserPostListView(ListView):
+    model = Post 
+    template_name = 'twigger/user_posts.html'  # <app>/<model>_<viewtype>.html  this is django convention
+    # we named our template differently so we need to set this variable
+    context_object_name = 'posts' # by default it is called objectList, we have diffrent name so we need to set it here
+    ordering = ['-date_posted']
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model = Post
