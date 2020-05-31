4fa2375796a1cfd1330046fb5d87899b505b4512
Abhishek <abhisheksinghal7599@gmail.com>
2020-05-31 17:54:39 +0530

    Add create post

--- twigger/urls.py
+++ twigger/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 from . import views
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
     # path('', views.home, name='twigger-home'),
     path('', PostListView.as_view(), name='twigger-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-'),
     path('about/', views.about, name = 'twigger-about')
+
     #path('about', views.about, name = 'twigger-about')
 ]
